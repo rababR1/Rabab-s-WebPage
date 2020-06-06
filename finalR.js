362b66c5a0e8149e9d185431abeef30910e0da94
rababR1 <65360255+rababR1@users.noreply.github.com>
2020-06-06 01:53:55 -0600

    Create webpage

---
+++ finalR.js
@@ -0,0 +1,14 @@
+/*function openNav() {
+    document.getElementById("nav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("nav").style.width = "0";
+  }*/
+  function openNav() {
+    document.getElementById("nav").style.width = "100%";
+  }
+  
+  /* Close/hide the sidenav */
+  function closeNav() {
+    document.getElementById("nav").style.width = "0";}
